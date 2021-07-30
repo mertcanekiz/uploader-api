a8925a2b0369838fcd80fbfa05f2a8b5cf4c3d6c
Mertcan Ekiz <ekiz.mertcan@gmail.com>
2021-07-30 11:55:51 +0300

    Add unit tests

--- Uploader.Application.UnitTests/Common/Exceptions/ValidationExceptionTest.cs
+++ Uploader.Application.UnitTests/Common/Exceptions/ValidationExceptionTest.cs
@@ -1,7 +1,54 @@
-﻿namespace Uploader.Application.UnitTests.Common.Exceptions
+﻿using System;
+using System.Collections.Generic;
+using FluentValidation.Results;
+using NUnit.Framework;
+using Uploader.Application.Common.Exceptions;
+
+namespace Uploader.Application.UnitTests.Common.Exceptions
 {
     public class ValidationExceptionTest
     {
-        
+        [Test]
+        public void Constructor_ShouldCreateAnEmptyErrorDictionary()
+        {
+            var actual = new ValidationException().Errors;
+            Assert.AreEqual(Array.Empty<string>(), actual.Keys);
+        }
+
+        [Test]
+        public void Constructor_ShouldCreateASingleElementErrorDictionary_WhenCalledWithSingleValidationFailure()
+        {
+            var failures = new List<ValidationFailure>
+            {
+                new ValidationFailure("Description", "must not be empty")
+            };
+            var actual = new ValidationException(failures).Errors;
+            Assert.AreEqual(new string[]{ "Description" }, actual.Keys);
+            Assert.AreEqual(new string[]{ "must not be empty" }, actual["Description"]);
+        }
+
+        [Test]
+        public void Constructor_ShouldCreateMultipleElementDictionary_WhenCalledWithMultipleValidationFailures()
+        {
+            var failures = new List<ValidationFailure>
+            {
+                new("Description", "must not be empty"),
+                new("Description", "must contain at most 500 characters"),
+                new("File", "must not be empty")
+            };
+
+            var actual = new ValidationException(failures).Errors;
+            
+            Assert.AreEqual(new string[]{ "Description", "File" }, actual.Keys);
+            Assert.AreEqual(new string[]
+            {
+                "must not be empty",
+                "must contain at most 500 characters"
+            }, actual["Description"]);
+            Assert.AreEqual(new string[]
+            {
+                "must not be empty"
+            }, actual["File"]);
+        }
     }
 }
